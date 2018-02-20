8a67961f88773add1df74849088c0feb5bddb114
nathangrr <nathanranthony@gmail.com>
2018-02-20 17:46:04 -0500

    PetDatabaseSearchable
    Added a file for PetDatabaseSearchable

---
+++ PetDatabaseSearchable.h
@@ -0,0 +1,18 @@
+#include "Pet.h"
+#include <vector>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#ifndef PETDATABASESEARCHABLE_H
+#define PETDATABASESEARCHABLE_H
+
+class PetDatabaseSearchable{
+public:
+    virtual unsigned int getSize() const = 0;
+    virtual int compareAt(int) const = 0;
+};
+
+
+#endif /* PETDATABASESEARCHABLE_H */
+
